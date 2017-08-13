1c392d182ef7055babf35774145f4f3f6c3f0bae
Pim de Wit <pim@pdw.io>
2017-08-13 14:55:40 +0100

    feat(*): *

--- src/scripts/core/constants/utilityAttributes.js
+++ src/scripts/core/constants/utilityAttributes.js
@@ -1,5 +1,15 @@
-const UTILITY_ATTRIBUTES = {
+/**
+ * @typedef {Object} UTILITY_ATTRIBUTES
+ * @property {String} POS_CENTERED Helper attribute to center-position an element.
+ */
 
+/**
+ * @type {UTILITY_ATTRIBUTES}
+ * @constant
+ */
+const UTILITY_ATTRIBUTES = {
+  POS_CENTERED: 'centered',
+  HIDDEN: 'visuallyhidden'
 };
 
 export default UTILITY_ATTRIBUTES;
---
+++ src/scripts/core/ui/modules/userselect.js
@@ -0,0 +1,28 @@
+import VENDOR_PREFIXES from '../../constants/vendorPrefixes.js';
+
+const VENDORS = [
+  VENDOR_PREFIXES.WEBKIT,
+  VENDOR_PREFIXES.MOZILLA,
+  VENDOR_PREFIXES.IE,
+  ''
+];
+
+class UserSelect {
+  constructor(element, value = 'none') {
+
+    VENDORS.forEach(vendor => {
+      element.style[`${vendor}user-select`] = value;
+    });
+
+  }
+}
+
+export default UserSelect;
+
+
+
+function setUserSelect(value) {
+  VENDORS.forEach(vendor => {
+    element.style[`${vendor}user-select`] = value;
+  });
+}
