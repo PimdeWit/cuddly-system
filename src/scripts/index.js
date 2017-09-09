5d22c4d3622e594c0ef071ae8d71a27e94097e25
Pim de Wit <pim@toasterltd.com>
2017-09-09 15:50:07 +0100

    chore(): Removed files which are currently out of scope

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,10 @@
 import '../styles/main.scss';
 import Boot from './boot/index';
-import Animation from './core/animations/index';
-// import Loader from './loaders/index';
+import {Load, Remove as RemoveFromCache, RemoveAll as RemoveAllFromCache} from './core/loaders/index';
 
 class Game {
   constructor() {
-    this.loader = null;
     this.boot = new Boot('../../images/lorem.png');
-
-    setTimeout(() => {
-      this.boot.dispose();
-      this.showMenu()
-    }, 2000);
   }
 
   showMenu() {
