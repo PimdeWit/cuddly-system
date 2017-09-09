228bf9f2febc412fdd3ab9d2b00aff584725e786
Pim de Wit <pim@toasterltd.com>
2017-09-09 15:49:00 +0100

    feat(loader): Added cache control methods

--- src/scripts/core/loaders/index.js
+++ src/scripts/core/loaders/index.js
@@ -1,5 +1,12 @@
-const _cache = {};
+/** @private */
+let _cache = {};
 
+/**
+ * Load an image, either from the cache or from
+ * @param path
+ * @returns {Promise}
+ * @constructor
+ */
 export function Load(path) {
   const blob = _cache[path];
 
@@ -10,6 +17,28 @@ export function Load(path) {
   }
 }
 
+/**
+ * Remove a key and its property from the cache.
+ * @param {String} key The key to remove.
+ */
+export function Remove(key) {
+  delete _cache[key];
+}
+
+/**
+ * Remove all keys and its properties from the cache.
+ * @constructor
+ */
+export function RemoveAll() {
+  _cache = {};
+}
+
+/**
+ * Fetch a file and return it.
+ * @param path
+ * @returns {Promise}
+ * @private
+ */
 function _fetchFile(path) {
   return new Promise((resolve, reject) => {
     fetch(path).then(response => {
